e8992ec0e79dd0cafa5cedeb8c1658edc3d7cf81
0xhatsume <0xhatsume@gmail.com>
2023-05-15 00:07:17 +0800

    feat: add bunch of components for customizer page

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useSnapshot } from 'valtio';
 import state from '../store';
 import { getContrastingColor } from '../config/helpers';
@@ -24,7 +24,7 @@ const CustomButton = ({type, title, customStyles, handleClick}) => {
     return (
         <button className={`px-2 py-1.5 flex-1 rounded-md" ${customStyles}`}
             style={generateStyle(type)}
-            onCLick={handleClick}
+            onClick={handleClick}
         >
             {title}
         </button>
